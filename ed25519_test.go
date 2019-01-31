5d30f9ba09940df3768d7292457b1ab9be1501b7
t-bast <bastien@stratumn.com>
2019-01-31 10:28:55 +0100

    Fix: Remove redundant Ed25519 public key (#36). The private key already contains the public key point, no need to add it twice.

--- ed25519_test.go
+++ ed25519_test.go
@@ -28,7 +28,7 @@ func TestBasicSignAndVerify(t *testing.T) {
 	}
 
 	if !ok {
-		t.Fatal("signature didnt match")
+		t.Fatal("signature didn't match")
 	}
 
 	// change data
@@ -70,59 +70,82 @@ func TestMarshalLoop(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	for name, f := range map[string]func() ([]byte, error){
-		"Bytes": priv.Bytes,
-		"Marshal": func() ([]byte, error) {
-			return MarshalPrivateKey(priv)
-		},
-		"NonRedundant": func() ([]byte, error) {
-			// Manually marshal a non-redundant private key to make sure it works!
-			pbmes := new(pb.PrivateKey)
-			pbmes.Type = priv.Type()
-			data, err := priv.Raw()
-			if err != nil {
-				t.Fatal(err)
-			}
+	t.Run("PrivateKey", func(t *testing.T) {
+		for name, f := range map[string]func() ([]byte, error){
+			"Bytes": priv.Bytes,
+			"Marshal": func() ([]byte, error) {
+				return MarshalPrivateKey(priv)
+			},
+			"Redundant": func() ([]byte, error) {
+				// See issue #36.
+				// Ed25519 private keys used to contain the public key twice.
+				// For backwards-compatibility, we need to continue supporting
+				// that scenario.
+				pbmes := new(pb.PrivateKey)
+				pbmes.Type = priv.Type()
+				data, err := priv.Raw()
+				if err != nil {
+					t.Fatal(err)
+				}
 
-			pbmes.Data = data[:ed25519.PrivateKeySize]
-			return pbmes.Marshal()
-		},
-	} {
-		t.Run(name, func(t *testing.T) {
-			bts, err := f()
-			if err != nil {
-				t.Fatal(err)
-			}
-			privNew, err := UnmarshalPrivateKey(bts)
-			if err != nil {
-				t.Fatal(err)
-			}
+				pbmes.Data = append(data, data[len(data)-ed25519.PublicKeySize:]...)
+				return pbmes.Marshal()
+			},
+		} {
+			t.Run(name, func(t *testing.T) {
+				bts, err := f()
+				if err != nil {
+					t.Fatal(err)
+				}
 
-			if !priv.Equals(privNew) || !privNew.Equals(priv) {
-				t.Fatal("keys are not equal")
-			}
-		})
-	}
+				privNew, err := UnmarshalPrivateKey(bts)
+				if err != nil {
+					t.Fatal(err)
+				}
 
-	for name, f := range map[string]func() ([]byte, error){
-		"Bytes": pub.Bytes,
-		"Marshal": func() ([]byte, error) {
-			return MarshalPublicKey(pub)
-		},
-	} {
-		t.Run(name, func(t *testing.T) {
-			bts, err := f()
-			if err != nil {
-				t.Fatal(err)
-			}
-			pubNew, err := UnmarshalPublicKey(bts)
-			if err != nil {
-				t.Fatal(err)
-			}
+				if !priv.Equals(privNew) || !privNew.Equals(priv) {
+					t.Fatal("keys are not equal")
+				}
 
-			if !pub.Equals(pubNew) || !pubNew.Equals(pub) {
-				t.Fatal("keys are not equal")
-			}
-		})
-	}
+				msg := []byte("My child, my sister,\nThink of the rapture\nOf living together there!")
+				signed, err := privNew.Sign(msg)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				ok, err := privNew.GetPublic().Verify(msg, signed)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if !ok {
+					t.Fatal("signature didn't match")
+				}
+			})
+		}
+	})
+
+	t.Run("PublicKey", func(t *testing.T) {
+		for name, f := range map[string]func() ([]byte, error){
+			"Bytes": pub.Bytes,
+			"Marshal": func() ([]byte, error) {
+				return MarshalPublicKey(pub)
+			},
+		} {
+			t.Run(name, func(t *testing.T) {
+				bts, err := f()
+				if err != nil {
+					t.Fatal(err)
+				}
+				pubNew, err := UnmarshalPublicKey(bts)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if !pub.Equals(pubNew) || !pubNew.Equals(pub) {
+					t.Fatal("keys are not equal")
+				}
+			})
+		}
+	})
 }
