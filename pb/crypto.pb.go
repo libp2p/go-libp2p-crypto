a7856f947ea0b901204806335a726fc07bed76cc
Jakub Sztandera <kubuxu@protonmail.ch>
2019-02-18 14:44:50 +0100

    Update protobuf

--- pb/crypto.pb.go
+++ pb/crypto.pb.go
@@ -3,12 +3,12 @@
 
 package crypto_pb
 
-import proto "github.com/gogo/protobuf/proto"
-import fmt "fmt"
-import math "math"
-
-import io "io"
-import github_com_gogo_protobuf_proto "github.com/gogo/protobuf/proto"
+import (
+	fmt "fmt"
+	proto "github.com/gogo/protobuf/proto"
+	io "io"
+	math "math"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -36,6 +36,7 @@ var KeyType_name = map[int32]string{
 	2: "Secp256k1",
 	3: "ECDSA",
 }
+
 var KeyType_value = map[string]int32{
 	"RSA":       0,
 	"Ed25519":   1,
@@ -43,38 +44,24 @@ var KeyType_value = map[string]int32{
 	"ECDSA":     3,
 }
 
-func (x KeyType) Enum() *KeyType {
-	p := new(KeyType)
-	*p = x
-	return p
-}
 func (x KeyType) String() string {
 	return proto.EnumName(KeyType_name, int32(x))
 }
-func (x *KeyType) UnmarshalJSON(data []byte) error {
-	value, err := proto.UnmarshalJSONEnum(KeyType_value, data, "KeyType")
-	if err != nil {
-		return err
-	}
-	*x = KeyType(value)
-	return nil
-}
+
 func (KeyType) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_crypto_916f6c71dcfaadb2, []int{0}
+	return fileDescriptor_527278fb02d03321, []int{0}
 }
 
 type PublicKey struct {
-	Type                 KeyType  `protobuf:"varint,1,req,name=Type,enum=crypto.pb.KeyType" json:"Type"`
-	Data                 []byte   `protobuf:"bytes,2,req,name=Data" json:"Data"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Type KeyType `protobuf:"varint,1,opt,name=Type,proto3,enum=crypto.pb.KeyType" json:"Type,omitempty"`
+	Data []byte  `protobuf:"bytes,2,opt,name=Data,proto3" json:"Data,omitempty"`
 }
 
 func (m *PublicKey) Reset()         { *m = PublicKey{} }
 func (m *PublicKey) String() string { return proto.CompactTextString(m) }
 func (*PublicKey) ProtoMessage()    {}
 func (*PublicKey) Descriptor() ([]byte, []int) {
-	return fileDescriptor_crypto_916f6c71dcfaadb2, []int{0}
+	return fileDescriptor_527278fb02d03321, []int{0}
 }
 func (m *PublicKey) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -91,8 +78,8 @@ func (m *PublicKey) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 		return b[:n], nil
 	}
 }
-func (dst *PublicKey) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PublicKey.Merge(dst, src)
+func (m *PublicKey) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PublicKey.Merge(m, src)
 }
 func (m *PublicKey) XXX_Size() int {
 	return m.Size()
@@ -118,17 +105,15 @@ func (m *PublicKey) GetData() []byte {
 }
 
 type PrivateKey struct {
-	Type                 KeyType  `protobuf:"varint,1,req,name=Type,enum=crypto.pb.KeyType" json:"Type"`
-	Data                 []byte   `protobuf:"bytes,2,req,name=Data" json:"Data"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Type KeyType `protobuf:"varint,1,opt,name=Type,proto3,enum=crypto.pb.KeyType" json:"Type,omitempty"`
+	Data []byte  `protobuf:"bytes,2,opt,name=Data,proto3" json:"Data,omitempty"`
 }
 
 func (m *PrivateKey) Reset()         { *m = PrivateKey{} }
 func (m *PrivateKey) String() string { return proto.CompactTextString(m) }
 func (*PrivateKey) ProtoMessage()    {}
 func (*PrivateKey) Descriptor() ([]byte, []int) {
-	return fileDescriptor_crypto_916f6c71dcfaadb2, []int{1}
+	return fileDescriptor_527278fb02d03321, []int{1}
 }
 func (m *PrivateKey) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -145,8 +130,8 @@ func (m *PrivateKey) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 		return b[:n], nil
 	}
 }
-func (dst *PrivateKey) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PrivateKey.Merge(dst, src)
+func (m *PrivateKey) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PrivateKey.Merge(m, src)
 }
 func (m *PrivateKey) XXX_Size() int {
 	return m.Size()
@@ -172,10 +157,30 @@ func (m *PrivateKey) GetData() []byte {
 }
 
 func init() {
+	proto.RegisterEnum("crypto.pb.KeyType", KeyType_name, KeyType_value)
 	proto.RegisterType((*PublicKey)(nil), "crypto.pb.PublicKey")
 	proto.RegisterType((*PrivateKey)(nil), "crypto.pb.PrivateKey")
-	proto.RegisterEnum("crypto.pb.KeyType", KeyType_name, KeyType_value)
 }
+
+func init() { proto.RegisterFile("crypto.proto", fileDescriptor_527278fb02d03321) }
+
+var fileDescriptor_527278fb02d03321 = []byte{
+	// 201 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x49, 0x2e, 0xaa, 0x2c,
+	0x28, 0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0xf1, 0x92, 0x94, 0xdc, 0xb9,
+	0x38, 0x03, 0x4a, 0x93, 0x72, 0x32, 0x93, 0xbd, 0x53, 0x2b, 0x85, 0xd4, 0xb8, 0x58, 0x42, 0x2a,
+	0x0b, 0x52, 0x25, 0x18, 0x15, 0x18, 0x35, 0xf8, 0x8c, 0x84, 0xf4, 0xe0, 0xca, 0xf4, 0xbc, 0x53,
+	0x2b, 0x41, 0x32, 0x41, 0x60, 0x79, 0x21, 0x21, 0x2e, 0x16, 0x97, 0xc4, 0x92, 0x44, 0x09, 0x26,
+	0x05, 0x46, 0x0d, 0x9e, 0x20, 0x30, 0x5b, 0xc9, 0x83, 0x8b, 0x2b, 0xa0, 0x28, 0xb3, 0x2c, 0xb1,
+	0x24, 0x95, 0x42, 0x93, 0xb4, 0x2c, 0xb9, 0xd8, 0xa1, 0x8a, 0x84, 0xd8, 0xb9, 0x98, 0x83, 0x82,
+	0x1d, 0x05, 0x18, 0x84, 0xb8, 0xb9, 0xd8, 0x5d, 0x53, 0x8c, 0x4c, 0x4d, 0x0d, 0x2d, 0x05, 0x18,
+	0x85, 0x78, 0xb9, 0x38, 0x83, 0x53, 0x93, 0x0b, 0x8c, 0x4c, 0xcd, 0xb2, 0x0d, 0x05, 0x98, 0x84,
+	0x38, 0xb9, 0x58, 0x5d, 0x9d, 0x5d, 0x82, 0x1d, 0x05, 0x98, 0x9d, 0x24, 0x4e, 0x3c, 0x92, 0x63,
+	0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96,
+	0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21, 0x89, 0x0d, 0xec, 0x73, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff,
+	0xff, 0x3f, 0x7a, 0x17, 0x34, 0x09, 0x01, 0x00, 0x00,
+}
+
 func (m *PublicKey) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -191,10 +196,12 @@ func (m *PublicKey) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	dAtA[i] = 0x8
-	i++
-	i = encodeVarintCrypto(dAtA, i, uint64(m.Type))
-	if m.Data != nil {
+	if m.Type != 0 {
+		dAtA[i] = 0x8
+		i++
+		i = encodeVarintCrypto(dAtA, i, uint64(m.Type))
+	}
+	if len(m.Data) > 0 {
 		dAtA[i] = 0x12
 		i++
 		i = encodeVarintCrypto(dAtA, i, uint64(len(m.Data)))
@@ -218,10 +225,12 @@ func (m *PrivateKey) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	dAtA[i] = 0x8
-	i++
-	i = encodeVarintCrypto(dAtA, i, uint64(m.Type))
-	if m.Data != nil {
+	if m.Type != 0 {
+		dAtA[i] = 0x8
+		i++
+		i = encodeVarintCrypto(dAtA, i, uint64(m.Type))
+	}
+	if len(m.Data) > 0 {
 		dAtA[i] = 0x12
 		i++
 		i = encodeVarintCrypto(dAtA, i, uint64(len(m.Data)))
@@ -240,22 +249,32 @@ func encodeVarintCrypto(dAtA []byte, offset int, v uint64) int {
 	return offset + 1
 }
 func (m *PublicKey) Size() (n int) {
+	if m == nil {
+		return 0
+	}
 	var l int
 	_ = l
-	n += 1 + sovCrypto(uint64(m.Type))
-	if m.Data != nil {
-		l = len(m.Data)
+	if m.Type != 0 {
+		n += 1 + sovCrypto(uint64(m.Type))
+	}
+	l = len(m.Data)
+	if l > 0 {
 		n += 1 + l + sovCrypto(uint64(l))
 	}
 	return n
 }
 
 func (m *PrivateKey) Size() (n int) {
+	if m == nil {
+		return 0
+	}
 	var l int
 	_ = l
-	n += 1 + sovCrypto(uint64(m.Type))
-	if m.Data != nil {
-		l = len(m.Data)
+	if m.Type != 0 {
+		n += 1 + sovCrypto(uint64(m.Type))
+	}
+	l = len(m.Data)
+	if l > 0 {
 		n += 1 + l + sovCrypto(uint64(l))
 	}
 	return n
@@ -275,7 +294,6 @@ func sozCrypto(x uint64) (n int) {
 	return sovCrypto(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
 func (m *PublicKey) Unmarshal(dAtA []byte) error {
-	var hasFields [1]uint64
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -290,7 +308,7 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 			}
 			b := dAtA[iNdEx]
 			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
+			wire |= uint64(b&0x7F) << shift
 			if b < 0x80 {
 				break
 			}
@@ -318,12 +336,11 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				m.Type |= (KeyType(b) & 0x7F) << shift
+				m.Type |= KeyType(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			hasFields[0] |= uint64(0x00000001)
 		case 2:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Data", wireType)
@@ -338,7 +355,7 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
+				byteLen |= int(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
@@ -347,6 +364,9 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 				return ErrInvalidLengthCrypto
 			}
 			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthCrypto
+			}
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
@@ -355,7 +375,6 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 				m.Data = []byte{}
 			}
 			iNdEx = postIndex
-			hasFields[0] |= uint64(0x00000002)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipCrypto(dAtA[iNdEx:])
@@ -365,17 +384,14 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 			if skippy < 0 {
 				return ErrInvalidLengthCrypto
 			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthCrypto
+			}
 			if (iNdEx + skippy) > l {
 				return io.ErrUnexpectedEOF
 			}
 			iNdEx += skippy
 		}
-	}
-	if hasFields[0]&uint64(0x00000001) == 0 {
-		return github_com_gogo_protobuf_proto.NewRequiredNotSetError("Type")
-	}
-	if hasFields[0]&uint64(0x00000002) == 0 {
-		return github_com_gogo_protobuf_proto.NewRequiredNotSetError("Data")
 	}
 
 	if iNdEx > l {
@@ -384,7 +400,6 @@ func (m *PublicKey) Unmarshal(dAtA []byte) error {
 	return nil
 }
 func (m *PrivateKey) Unmarshal(dAtA []byte) error {
-	var hasFields [1]uint64
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -399,7 +414,7 @@ func (m *PrivateKey) Unmarshal(dAtA []byte) error {
 			}
 			b := dAtA[iNdEx]
 			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
+			wire |= uint64(b&0x7F) << shift
 			if b < 0x80 {
 				break
 			}
@@ -427,12 +442,11 @@ func (m *PrivateKey) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				m.Type |= (KeyType(b) & 0x7F) << shift
+				m.Type |= KeyType(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			hasFields[0] |= uint64(0x00000001)
 		case 2:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Data", wireType)
@@ -447,7 +461,7 @@ func (m *PrivateKey) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
+				byteLen |= int(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
@@ -456,6 +470,9 @@ func (m *PrivateKey) Unmarshal(dAtA []byte) error {
 				return ErrInvalidLengthCrypto
 			}
 			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthCrypto
+			}
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
@@ -464,7 +481,6 @@ func (m *PrivateKey) Unmarshal(dAtA []byte) error {
 				m.Data = []byte{}
 			}
 			iNdEx = postIndex
-			hasFields[0] |= uint64(0x00000002)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipCrypto(dAtA[iNdEx:])
@@ -474,17 +490,14 @@ func (m *PrivateKey) Unmarshal(dAtA []byte) error {
 			if skippy < 0 {
 				return ErrInvalidLengthCrypto
 			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthCrypto
+			}
 			if (iNdEx + skippy) > l {
 				return io.ErrUnexpectedEOF
 			}
 			iNdEx += skippy
 		}
-	}
-	if hasFields[0]&uint64(0x00000001) == 0 {
-		return github_com_gogo_protobuf_proto.NewRequiredNotSetError("Type")
-	}
-	if hasFields[0]&uint64(0x00000002) == 0 {
-		return github_com_gogo_protobuf_proto.NewRequiredNotSetError("Data")
 	}
 
 	if iNdEx > l {
@@ -546,8 +559,11 @@ func skipCrypto(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			iNdEx += length
 			if length < 0 {
+				return 0, ErrInvalidLengthCrypto
+			}
+			iNdEx += length
+			if iNdEx < 0 {
 				return 0, ErrInvalidLengthCrypto
 			}
 			return iNdEx, nil
@@ -578,6 +594,9 @@ func skipCrypto(dAtA []byte) (n int, err error) {
 					return 0, err
 				}
 				iNdEx = start + next
+				if iNdEx < 0 {
+					return 0, ErrInvalidLengthCrypto
+				}
 			}
 			return iNdEx, nil
 		case 4:
@@ -596,22 +615,3 @@ var (
 	ErrInvalidLengthCrypto = fmt.Errorf("proto: negative length found during unmarshaling")
 	ErrIntOverflowCrypto   = fmt.Errorf("proto: integer overflow")
 )
-
-func init() { proto.RegisterFile("crypto.proto", fileDescriptor_crypto_916f6c71dcfaadb2) }
-
-var fileDescriptor_crypto_916f6c71dcfaadb2 = []byte{
-	// 194 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x49, 0x2e, 0xaa, 0x2c,
-	0x28, 0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0xf1, 0x92, 0x94, 0x82, 0xb9,
-	0x38, 0x03, 0x4a, 0x93, 0x72, 0x32, 0x93, 0xbd, 0x53, 0x2b, 0x85, 0x74, 0xb8, 0x58, 0x42, 0x2a,
-	0x0b, 0x52, 0x25, 0x18, 0x15, 0x98, 0x34, 0xf8, 0x8c, 0x84, 0xf4, 0xe0, 0xca, 0xf4, 0xbc, 0x53,
-	0x2b, 0x41, 0x32, 0x4e, 0x2c, 0x27, 0xee, 0xc9, 0x33, 0x04, 0x81, 0x55, 0x09, 0x49, 0x70, 0xb1,
-	0xb8, 0x24, 0x96, 0x24, 0x4a, 0x30, 0x29, 0x30, 0x69, 0xf0, 0xc0, 0x64, 0x40, 0x22, 0x4a, 0x21,
-	0x5c, 0x5c, 0x01, 0x45, 0x99, 0x65, 0x89, 0x25, 0xa9, 0x54, 0x34, 0x55, 0xcb, 0x92, 0x8b, 0x1d,
-	0xaa, 0x41, 0x88, 0x9d, 0x8b, 0x39, 0x28, 0xd8, 0x51, 0x80, 0x41, 0x88, 0x9b, 0x8b, 0xdd, 0x35,
-	0xc5, 0xc8, 0xd4, 0xd4, 0xd0, 0x52, 0x80, 0x51, 0x88, 0x97, 0x8b, 0x33, 0x38, 0x35, 0xb9, 0xc0,
-	0xc8, 0xd4, 0x2c, 0xdb, 0x50, 0x80, 0x49, 0x88, 0x93, 0x8b, 0xd5, 0xd5, 0xd9, 0x25, 0xd8, 0x51,
-	0x80, 0xd9, 0x49, 0xe0, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63,
-	0x9c, 0xf0, 0x58, 0x8e, 0x01, 0x10, 0x00, 0x00, 0xff, 0xff, 0xf1, 0xcc, 0xed, 0xac, 0x11, 0x01,
-	0x00, 0x00,
-}
