6736dbfa1f0a09a11539f91b1a76cd969caa7339
Raúl Kripalani <raul@protocol.ai>
2019-05-23 16:06:57 +0100

    crypto test utils now moved to core.

--- go.mod
+++ go.mod
@@ -1,6 +1,3 @@
 module github.com/libp2p/go-libp2p-crypto
 
-require (
-	github.com/libp2p/go-libp2p-core v0.0.0-20190508144953-ed42958fbb3a
-	github.com/libp2p/go-libp2p-testing v0.0.0-20190508172549-1a0da3de1915
-)
+require github.com/libp2p/go-libp2p-core v0.0.0-20190508144953-ed42958fbb3a
--- test/deprecated.go
+++ test/deprecated.go
@@ -1,14 +1,14 @@
 package testutil
 
 import (
+	"github.com/libp2p/go-libp2p-core/test"
 	ci "github.com/libp2p/go-libp2p-crypto"
-	"github.com/libp2p/go-libp2p-testing/crypto"
 )
 
 func RandTestKeyPair(typ, bits int) (ci.PrivKey, ci.PubKey, error) {
-	return tcrypto.RandTestKeyPair(typ, bits)
+	return test.RandTestKeyPair(typ, bits)
 }
 
 func SeededTestKeyPair(typ, bits int, seed int64) (ci.PrivKey, ci.PubKey, error) {
-	return tcrypto.SeededTestKeyPair(typ, bits, seed)
+	return test.SeededTestKeyPair(typ, bits, seed)
 }
