a76f624aa1ed940a7a0cf17f12a97c5ef9a6be8f
Raúl Kripalani <raul@protocol.ai>
2019-05-23 16:36:07 +0100

    add missing deprecation notices.

--- test/deprecated.go
+++ test/deprecated.go
@@ -1,3 +1,4 @@
+// Deprecated: use github.com/libp2p/go-libp2p-core/test instead.
 package testutil
 
 import (
@@ -5,10 +6,12 @@ import (
 	ci "github.com/libp2p/go-libp2p-crypto"
 )
 
+// Deprecated: use github.com/libp2p/go-libp2p-core/test.RandTestKeyPair instead.
 func RandTestKeyPair(typ, bits int) (ci.PrivKey, ci.PubKey, error) {
 	return test.RandTestKeyPair(typ, bits)
 }
 
+// Deprecated: use github.com/libp2p/go-libp2p-core/test.SeededTestKeyPair instead.
 func SeededTestKeyPair(typ, bits int, seed int64) (ci.PrivKey, ci.PubKey, error) {
 	return test.SeededTestKeyPair(typ, bits, seed)
 }
